a53f978f8685efc194ae17eb7df6bd14b758e865
nouheera <n201100@rguktn.ac.in>
2026-01-07 13:32:53 +0530

    Add admin stock page

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -18,3 +18,10 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
+
+router.put("/:id", async (req, res) => {
+  const { stock } = req.body;
+
+  await Product.findByIdAndUpdate(req.params.id, { stock });
+  res.json({ success: true });
+});
